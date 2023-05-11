a2d4ce26ff87a6d0d7f4849fce415dff97424709
jlphoenix9 <20033909@myrp.edu.sg>
2023-05-11 15:17:00 +0800

    2nd commit

--- app/src/main/java/sg/edu/rp/c346/id20033909/l04reservation/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20033909/l04reservation/MainActivity.java
@@ -3,12 +3,35 @@ package sg.edu.rp.c346.id20033909.l04reservation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
+    EditText name;
+    EditText mobile;
+    EditText size;
+    TimePicker tp;
+    DatePicker dp;
+    RadioGroup radio;
+    Button confirm;
+    Button reset;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        name = findViewById(R.id.name);
+        dp = findViewById(R.id.datePicker);
+        tp = findViewById(R.id.timePicker);
+        confirm = findViewById(R.id.confirm);
+        reset = findViewById(R.id.reset);
+
+
     }
 }
